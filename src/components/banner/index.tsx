f14b2fd35db5c465765ec8308a589e0c04935a79
can101 <can896373@gmail.com>
2022-12-26 04:06:44 +0300

    feat(banner) banner style and banner counter completed

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -1,10 +1,33 @@
-import React, { FC, ReactElement, Fragment } from 'react'
+import React, {useState, FC, ReactElement, useLayoutEffect} from 'react'
 import './style.css'
 
 const Banner: FC = (): ReactElement => {
+    const [differenceDays, setDifferenceDays] = useState(0);
+    const [differenceHours, setDifferenceHours] = useState(0);
+    const [differenceMinutes, setDifferenceMinutes] = useState(0);
+    const [differenceSeconds, setDifferenceSeconds] = useState(0);
+    const timeCalculator = () => {
+       setInterval( ()=>{
+            const countDownDate = new Date(new Date().getUTCFullYear(), 11, 31).getTime();
+            var now = new Date().getTime();
+            var distance = countDownDate - now;
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+           setDifferenceHours(hours)
+           setDifferenceDays(days)
+           setDifferenceMinutes(minutes)
+           setDifferenceSeconds(seconds)
+            console.log(days, hours, minutes, seconds)
+        },1000)
+    }
+    useLayoutEffect(() => {
+        timeCalculator();
+    }, [])
     return (
         <div className='banner-container'>
-            <span>Banner</span>
+            <span>Yılbaşına son <span className={"banner-text-highlight"}> {differenceDays} gün {differenceHours} saat {differenceMinutes} dakika {differenceSeconds} saniye kaldı</span></span>
         </div>
     )
 }
