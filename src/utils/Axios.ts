942bca7ce386bdc94418f7222755dfc2221afa07
can101 <can896373@gmail.com>
2022-12-28 22:51:20 +0300

    fix : axios cors problem

--- src/utils/Axios.ts
+++ src/utils/Axios.ts
@@ -1,13 +1,8 @@
 import Axios from "axios";
 
 const instance = Axios.create({
-    baseURL: "https://swapi.dev/api/",
-    headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Access-Control-Allow-Headers, Content-Type, Authorization',
-        'Access-Control-Allow-Methods': '*',
-        "Content-Type": "application/json"
-    }
+    baseURL: "https://swapi.py4e.com/api/",
+    headers: {}
 });
 
 export default instance;
