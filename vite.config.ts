7a0274aa5567df2c65c7e931a3d4a9a092f13519
can101 <can896373@gmail.com>
2022-12-28 22:00:26 +0300

    style(url) change api url

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-server:{
-  proxy:{
-    '/api': {
-      target: 'https://swapi.dev/api/',
-      changeOrigin: true,
-      rewrite: (path) => path.replace(/^\/api/, ''),
-    },
-  }
-},
-  plugins: [react()]
+    plugins: [react()]
 })
