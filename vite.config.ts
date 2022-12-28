4b2690dabb9c2ddddbea7514d4a2371988e86abb
can101 <can896373@gmail.com>
2022-12-28 06:43:54 +0300

    feat(proxy) add proxy api url

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+server:{
+  proxy:{
+    '/api': {
+      target: 'https://swapi.dev/api/',
+      changeOrigin: true,
+      rewrite: (path) => path.replace(/^\/api/, ''),
+    },
+  }
+},
   plugins: [react()]
 })
