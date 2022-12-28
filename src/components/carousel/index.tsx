92b4ac89522d6781f4b9269972405d97531ddf8d
can101 <can896373@gmail.com>
2022-12-28 06:48:01 +0300

    style(redux) modified some codes

--- src/components/carousel/index.tsx
+++ src/components/carousel/index.tsx
@@ -1,20 +1,30 @@
-import React, {FC, ReactElement} from 'react'
+import React, {FC, ReactElement, ReactNode, useEffect} from 'react'
 import Video from '../../assets/videos/bg-video.mp4'
 // @ts-ignore
 import Slider from "react-slick";
 import './style.css'
 import Text from '../ui/textures/carousel-text'
 import DownButton from "../ui/buttons/down-button";
+import {useDispatch, useSelector} from 'react-redux'
+import filmAsyncThunk from "../../features/filmFeature/filmAsyncThunk"
 
 const Carousel: FC = (): ReactElement => {
+    const dispatch = useDispatch();
+    // @ts-ignore
+    const {value, loading} = useSelector(state => state.films)
+    useEffect(() => {
+        // @ts-ignore
+        dispatch(filmAsyncThunk())
+    }, [])
+
     const settings = {
         dots: false,
         arrows: false,
         centerMode: true,
         slidesToShow: 3,
         autoplay: true,
-        centerPadding: "30px",
-        autoplaySpeed: 6600
+        centerPadding: "0px",
+        autoplaySpeed: 6600,
     };
     return (
         <>
@@ -22,16 +32,21 @@ const Carousel: FC = (): ReactElement => {
                 <video src={Video} className={"video-container"} autoPlay loop playsInline muted/>
                 <div className={"image-carousel-wrapper"}>
                     <Slider {...settings}>
-                        <Text/>
-                        <Text/>
-                        <Text/>
-                        <Text/>
-                        <Text/>
-                        <Text/>
-                        <Text/>
+                        {value.map((item: any, index: number) => {
+                            return (
+                                <Text key={index} directory={item?.director} title={item?.title}
+                                      episode={`episode ${item?.episode_id}`}/>
+                            );
+                        })}
                     </Slider>
                 </div>
-                <DownButton/>
+                <DownButton click={() => {
+                    // @ts-ignore
+                    // document.getElementsByClassName("section-second").scrollIntoView()
+                    const target = document.querySelector('.target-section');
+                    // @ts-ignore
+                    target.scrollIntoView({behavior: 'smooth'});
+                }}/>
             </div>
         </>
 
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,32 @@
-import React, { FC, ReactElement } from 'react'
+import React, {FC, ReactElement} from 'react'
 import Carousel from '../../components/carousel'
-
+import imgPath from '../../assets/images/png/a.png'
+import './style.css'
 
 const index: FC = (): ReactElement => {
     return (
         <div>
             <Carousel/>
+            <section className="section-second target-section">
+                <div>
+                    <p>
+                        Star Wars is a science fiction media franchise created by George Lucas. It includes a series of
+                        films, television shows, and other works set in a galaxy far, far away and featuring a diverse
+                        cast
+                        of characters, including humans, droids, and various alien species. The franchise centers around
+                        a
+                        group of rebels fighting against an evil empire, led by the Sith Lord Darth Vader, and includes
+                        themes of redemption, family, and the struggle between good and evil. The original trilogy,
+                        consisting of the films "Star Wars: A New Hope," "The Empire Strikes Back," and "Return of the
+                        Jedi," was released between 1977 and 1983 and became a cultural phenomenon, influencing many
+                        aspects
+                        of popular culture and inspiring numerous spin-off works. The franchise has continued with
+                        numerous
+                        prequel, sequel, and anthology films, as well as television shows, video games, and other media.
+                    </p>
+                </div>
+                <img src={imgPath} className={"right-float-img"} alt="star-wars"/>
+            </section>
         </div>
     )
 }
