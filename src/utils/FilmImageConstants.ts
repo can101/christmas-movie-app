e44ee506ecbca8ad0d14b0a22cf35142325977da
can101 <can896373@gmail.com>
2022-12-31 00:07:20 +0300

    feat : add film image constants array url

---
+++ src/utils/FilmImageConstants.ts
@@ -0,0 +1,13 @@
+import firstImage from "../assets/images/png/films/a-new-hope.png";
+import secondImage from "../assets/images/png/films/the-empire-strikes-back.png";
+import thirdImage from "../assets/images/png/films/return-of-jedi.png";
+import fourthImage from "../assets/images/png/films/the-phathom-menace.png";
+import fifthImage from "../assets/images/png/films/atack-of-the-clones.png";
+import sixthImage from "../assets/images/png/films/revenge-of-the-stih.png";
+import seventhImage from "../assets/images/png/films/the-force-awakens.png";
+
+
+export default [
+    firstImage, secondImage, thirdImage, fourthImage, fifthImage, sixthImage, seventhImage
+]
+
