d76b63fc5a7b5dc35bc06c2df171d0653d6f6966
can101 <can896373@gmail.com>
2022-12-30 23:42:26 +0300

    feat : add new reducer

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,9 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit'
 import filmReducer from './filmFeature'
+import filmFoundReducer from "./filmFoundFeatures";
 
 
 export const store = configureStore({
     reducer: {
-        films: filmReducer
+        films: filmReducer,
+        found: filmFoundReducer
     },
+    middleware:(getDefaultMiddleware)=>getDefaultMiddleware({serializableCheck: false,})
 })
