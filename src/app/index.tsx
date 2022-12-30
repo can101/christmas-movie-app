bbb309395f6221a5999df0893e7714f55b14b31c
can101 <can896373@gmail.com>
2022-12-30 23:43:20 +0300

    feat : add loading animate

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,20 +1,23 @@
 import './style.css'
-import React, {useEffect} from 'react'
+import React, {useEffect, useState,lazy,Suspense} from 'react'
 import {snowGenerator} from '../assets/scripts/snow'
-import Home from '../pages/home'
 import {RouterProvider} from "react-router-dom";
 import router from "../routes";
-
+import Loading from '../components/loading'
 
 function App() {
-
+    const [isLoading, setIsLoading] = useState(true)
     useEffect(() => {
         snowGenerator()
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 3000);
     }, [])
 
     return (
         <div className="App">
-            <RouterProvider router={router} />
+            {isLoading && <Loading/>}
+            {!isLoading && <RouterProvider router={router}/>}
         </div>
     )
 }
