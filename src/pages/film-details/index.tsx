0f296688e1a178fffd2cde4de87cb302384ddc9f
can101 <can896373@gmail.com>
2023-01-02 05:09:02 +0300

    style : add details components

--- src/pages/film-details/index.tsx
+++ src/pages/film-details/index.tsx
@@ -1,24 +1,46 @@
 import React, {FC, ReactElement, useEffect} from 'react'
 import './style.css'
-import {useDispatch} from "react-redux";
-import {useParams} from 'react-router-dom'
+import {useDispatch, useSelector} from "react-redux";
+import {useNavigate, useParams} from 'react-router-dom'
 import filmFoundAsyncThunk from '../../features/filmFoundFeatures/filmFoundAsyncThunk'
+import HeroContent from "../../components/hero-content";
+import {resetState} from "../../features/filmFoundFeatures/index";
+import {BsArrowLeftSquare} from 'react-icons/bs'
+import IconButton from "../../components/ui/buttons/icon-button";
+import Spinner from "../../components/ui/spinner";
+import Carousel from "../../components/detail-carousel";
+import Detail from '../../components/movie-detail'
 
 const index: FC = (): ReactElement => {
+    const navigate = useNavigate()
     const {id} = useParams();
     const dispatch = useDispatch();
     useEffect(() => {
         // @ts-ignore
         dispatch(filmFoundAsyncThunk(Number(id)))
-        // dispatch(foundFilmDetails(id));
-        // return () => {
-        //     dispatch(resetDetailsState());
-        // };
+        return () => {
+            // @ts-ignore
+            dispatch(resetState());
+        };
     }, []);
+    const {loading, value} = useSelector((state: any) => state.found);
 
     return (
         <div className={"details-container"}>
-            hello
+            {loading && <Spinner/>}
+            {!loading && (<>
+                <div className={"back-button-wrapper"}>
+                    <IconButton icon={<BsArrowLeftSquare/>} click={() => navigate(-1)}/>
+                </div>
+                <HeroContent {...{
+                    title: value.title,
+                    description: value.opening_crawl,
+                    img_source: value.image,
+                    date: value.release_date
+                }}/>
+                <Carousel/>
+                <Detail/>
+            </>)}
         </div>
     )
 }
