740af759902d923bdb6b5b8dccf9c61196fba0b4
can101 <can896373@gmail.com>
2023-01-02 07:37:26 +0300

    feat : add error route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,12 +5,14 @@ import Home from "../pages/home";
 import MainLayout from "../components/layouts/mainLayout";
 import Search from "../pages/search";
 import FilmDetails from "../pages/film-details";
+import Error from "../pages/error";
 
 const router = createBrowserRouter([
 
     {
         path: "/",
         element: <MainLayout/>,
+        errorElement: <Error/>,
         children: [
             {
                 path: "/",
