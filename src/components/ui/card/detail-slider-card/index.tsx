12d0164d04ea3259a0540775aa7251a8a42a683d
can101 <can896373@gmail.com>
2023-01-02 05:06:15 +0300

    feat : add detail slider card component

---
+++ src/components/ui/card/detail-slider-card/index.tsx
@@ -0,0 +1,24 @@
+import React, {FC, ReactElement, ReactNode} from 'react'
+// @ts-ignore
+import './style.css'
+
+interface sliderCardProps {
+    length: number,
+    source_url: string,
+    title: string
+}
+
+const Carousel: FC<sliderCardProps> = ({length, source_url, title}): ReactElement => {
+    return (
+        <div className={"card-box"}>
+            <div className={"card-content"}>
+                <div> {title}</div>
+                {length || 0}
+            </div>
+            <img loading={"lazy"} className={"card-image"} src={source_url}/>
+        </div>
+
+    )
+}
+
+export default Carousel
