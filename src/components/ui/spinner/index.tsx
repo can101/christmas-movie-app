1380ce0ffc653d04ec4a4cfd8179a9bb7bee556d
can101 <can896373@gmail.com>
2023-01-02 04:58:09 +0300

    feat : add spinner component

---
+++ src/components/ui/spinner/index.tsx
@@ -0,0 +1,12 @@
+import React, {FC, ReactElement} from 'react'
+import './style.css'
+
+interface textProps {
+
+}
+
+const Carousel: FC = (): ReactElement => {
+    return <span className={"spinner-box"}></span>
+}
+
+export default Carousel
