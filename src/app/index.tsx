04a187506f2df67cc5243ea4db9d90fb6391f5a3
can101 <can896373@gmail.com>
2023-01-11 00:50:21 +0300

    feat: use goto top component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,21 +1,35 @@
 import './style.css'
-import React, {useEffect, useState,lazy,Suspense} from 'react'
+import React, {useEffect, useState, useRef} from 'react'
 import {snowGenerator} from '../assets/scripts/snow'
 import {RouterProvider} from "react-router-dom";
 import router from "../routes";
 import Loading from '../components/loading'
+import GoToTop from "../components/ui/buttons/go-to-top-button";
 
 function App() {
-    const [isLoading, setIsLoading] = useState(true)
+    const [isLoading, setIsLoading] = useState(true);
+    const [isSnowing, setIsSnowing] = useState(false);
+    const ref = useRef<HTMLButtonElement>(null);
     useEffect(() => {
         snowGenerator()
         setTimeout(() => {
             setIsLoading(false);
         }, 3000);
+        window.addEventListener('scroll', () => {
+            const scroll = window.scrollY;
+            console.log(scroll)
+            if (scroll > 600) {
+                setIsSnowing(true)
+            } else {
+                setIsSnowing(false)
+            }
+        })
     }, [])
 
     return (
         <div className="App">
+            {isSnowing && <GoToTop/>}
+            {/*<GoToTop/>*/}
             {isLoading && <Loading/>}
             {!isLoading && <RouterProvider router={router}/>}
         </div>
