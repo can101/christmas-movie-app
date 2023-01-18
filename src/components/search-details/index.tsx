3855337e702763d25e53032285df663f70892a06
can101 <can896373@gmail.com>
2023-01-18 13:37:04 +0300

    feat: add search details component

---
+++ src/components/search-details/index.tsx
@@ -0,0 +1,23 @@
+import react from "react";
+import "./style.css";
+import {useSelector} from "react-redux";
+import Card from "../cards/card";
+import React from "react";
+
+export default function SearchDetails() {
+    const {value, searchBy} = useSelector((state: any) => state.search);
+    return (
+        <div className="search-details-container">
+            <div className="search-details__description">{value.length} result founded</div>
+            <div className="search-details">
+                {
+                    value.map((item: any, index: number) => {
+                        return (
+                            <Card item={item} title={item?.name || item?.title} isReverse={~~index % 2 == 0 ? false : true} key={index} index={index} searchBy={searchBy}/>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    );
+}
