d7c1ad8e1f278b73514e6f9fc439df86e9431b03
can101 <can896373@gmail.com>
2023-01-18 23:46:55 +0300

    fix: redux devtools production disable

--- src/features/index.ts
+++ src/features/index.ts
@@ -3,12 +3,14 @@ import filmReducer from './filmFeature'
 import filmFoundReducer from "./filmFoundFeatures";
 import searchReducer from "./searchFeature";
 
+console.log(import.meta.env.VITE_NODE_ENV)
+
 export const store = configureStore({
     reducer: {
         films: filmReducer,
         found: filmFoundReducer,
         search: searchReducer
     },
-    middleware:(getDefaultMiddleware)=>getDefaultMiddleware({serializableCheck: false,}),
-    devTools: import.meta.env.NODE_ENV !== "production",
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false,}),
+    devTools: import.meta.env.VITE_NODE_ENV === 'development',
 })
